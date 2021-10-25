5188b78332e6562e09a8783568b529b76bfbf8ce
alezalez8 <5446943@ukr.net>
2021-10-25 19:45:23 +0300

    save_work_3

--- src/com/company/concurrency_lessons/vasko/ConcurrentMain.java
+++ src/com/company/concurrency_lessons/vasko/ConcurrentMain.java
@@ -11,21 +11,38 @@ public class ConcurrentMain {
         SimpleThread th2 = new SimpleThread();
         th2.start();
 
+        Thread th3 = new Thread((new SimpleRunner()));
+        th3.start();
+        System.out.println("break");
+
     }
 }
 
 class SimpleThread extends Thread {
     @Override
     public void run() {
-
-
         for (int i = 0; i < 10; i++) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 System.out.println(RED + "WARN - " + currentThread().getName() + " was interrupted ");
             }
-            System.out.println(GREEN + "INFO - " + currentThread().getName() + " - " + i);
+            System.out.println(GREEN + "INFO - Runnable  " + currentThread().getName() + " - " + i);
         }
+    }
+}
+
+class SimpleRunner implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(RED + "WARN - " + Thread.currentThread().getName() + " was interrupted ");
+            }
+            System.out.println(CYAN  + "INFO - " + Thread.currentThread().getName() + " - " + i);
+        }
+
     }
 }
