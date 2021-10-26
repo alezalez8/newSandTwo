3bd782ab248547c33047975f767008a171c0a7a7
alezalez8 <5446943@ukr.net>
2021-10-26 19:43:44 +0300

    daemon potoki

--- src/com/company/concurrency_lessons/vasko/GCDRunnable.java
+++ src/com/company/concurrency_lessons/vasko/GCDRunnable.java
@@ -1,4 +1,42 @@
 package com.company.concurrency_lessons.vasko;
 
-public class GCDRunnable {
+import java.util.Random;
+
+import static com.company.concurrency_lessons.vasko.ColorScheme.BLUE;
+import static com.company.concurrency_lessons.vasko.ColorScheme.GREEN;
+
+public class GCDRunnable extends Random implements Runnable {
+
+
+    public GCDRunnable() {
+    }
+
+    @Override
+    public void run() {
+        String threadDescription = Thread.currentThread().getName();
+
+        System.out.println(BLUE + "Starting " + threadDescription);
+        for (int i = 0; i < 10000000; i++) {
+            int a = nextInt();
+            int b = nextInt();
+
+            if (i % 10000 == 0) {
+                int gcd = computeGCD(a, b);
+                if (gcd > 5) {
+                    System.out.println(GREEN + "Running in " + threadDescription + ". The GCD of "
+                            + a + " and" + b + " is " + gcd);
+
+                }
+            }
+        }
+        System.out.println(BLUE + "Leaving the thread " + threadDescription);
+    }
+
+    private int computeGCD(int number1, int number2) {
+        if (number2 == 0) {
+            return number1;
+        } else {
+            return computeGCD(number2, number1 % number2);
+        }
+    }
 }
