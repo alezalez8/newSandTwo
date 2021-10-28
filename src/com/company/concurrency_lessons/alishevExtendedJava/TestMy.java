b9481db665b797a6f7a24690d6292ff1ce562ef2
alezalez8 <5446943@ukr.net>
2021-10-28 14:39:48 +0300

    alishev volatile end

--- src/com/company/concurrency_lessons/alishevExtendedJava/TestMy.java
+++ src/com/company/concurrency_lessons/alishevExtendedJava/TestMy.java
@@ -1,27 +1,35 @@
 package com.company.concurrency_lessons.alishevExtendedJava;
 
+import com.company.concurrency_lessons.vasko.ColorScheme;
+
+import static com.company.concurrency_lessons.vasko.ColorScheme.*;
+
 public class TestMy {
     public static void main(String[] args) {
+
 
         MyThreadClass myThreadClass = new MyThreadClass();
         myThreadClass.start();
 
         Thread myThreadClassTwo = new Thread(new ExRun());
         myThreadClassTwo.start();
-
+        System.out.println( YELLOW + "Main  name of thread is " +"\"" + Thread.currentThread().getName() + "\"");
+        System.out.printf(BLUE + "%s Main  name of thread %n", Thread.currentThread().getName());
     }
 }
 
 class MyThreadClass extends Thread {
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Thread_one " + i);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(GREEN+ "Thread_one " + i + "  name of thread is " + Thread.currentThread().getName());
+/*
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+
+            } */
         }
     }
 }
@@ -29,13 +37,15 @@ class MyThreadClass extends Thread {
 class ExRun implements Runnable {
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Thread_two " + i);
-            try {
+        for (int i = 0; i < 100; i++) {
+
+            System.out.println(RED + "Thread_two " + i + "  name of thread is " + Thread.currentThread().getName());
+ /*           try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+*/
         }
     }
 }
