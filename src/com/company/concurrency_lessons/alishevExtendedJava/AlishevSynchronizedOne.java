06679baff0f57d6e9accf961f2b620ec6f640139
alezalez8 <5446943@ukr.net>
2021-10-28 21:39:03 +0300

    alishev synchro 2 end

--- src/com/company/concurrency_lessons/alishevExtendedJava/AlishevSynchronizedOne.java
+++ src/com/company/concurrency_lessons/alishevExtendedJava/AlishevSynchronizedOne.java
@@ -4,11 +4,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class AlishevSynchronizedOne {
     private int counter;
-    private AtomicInteger count = new AtomicInteger(0);
+    //private AtomicInteger count = new AtomicInteger(0);
 
     public static void main(String[] args) throws InterruptedException {
         AlishevSynchronizedOne test = new AlishevSynchronizedOne();
         test.doWork();
+    }
+
+    public void increment() {
+        synchronized (this) {
+            counter++;
+        }
     }
 
     public void doWork() throws InterruptedException {
@@ -26,7 +32,6 @@ public class AlishevSynchronizedOne {
                 for (int i = 0; i < 10000; i++) {
                     //  counter += 1;
                     increment();
-
                     // count.incrementAndGet();
                 }
             }
@@ -39,10 +44,6 @@ public class AlishevSynchronizedOne {
         thread2.join();
 
         System.out.println(counter);
-        System.out.println(count);
-    }
-
-    public synchronized void increment() {
-        counter++;
+       // System.out.println(count);
     }
 }
