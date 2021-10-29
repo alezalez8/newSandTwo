8dc6733846d797a55c07bad1bdeb5bd6677d9feb
alezalez8 <5446943@ukr.net>
2021-10-29 14:26:40 +0300

    alishev threadpool begin

---
+++ src/com/company/concurrency_lessons/alishevExtendedJava/TestThreadPool.java
@@ -0,0 +1,23 @@
+package com.company.concurrency_lessons.alishevExtendedJava;
+
+public class TestThreadPool {
+
+}
+
+class Work implements Runnable {
+    private int id;
+
+    public Work(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Hello");
+    }
+}
