fe047776bfaeb41bd5e96c7447d99a445337663c
alezalez8 <5446943@ukr.net>
2021-10-29 15:02:04 +0300

    threadpool end

--- src/com/company/concurrency_lessons/alishevExtendedJava/TestThreadPool.java
+++ src/com/company/concurrency_lessons/alishevExtendedJava/TestThreadPool.java
@@ -2,10 +2,23 @@ package com.company.concurrency_lessons.alishevExtendedJava;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class TestThreadPool {
-    ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 5; i++)
+            executorService.submit(new Work(i));
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+        System.out.println("All tasks submitted");
+
+    }
 }
+
 
 class Work implements Runnable {
     private int id;
