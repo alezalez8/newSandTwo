0d4c60d75735bc4b2d45a24912b62b82117a5c3f
alezalez8 <5446943@ukr.net>
2021-10-31 22:21:04 +0200

    alishev Semahpore 12.00

--- src/com/company/concurrency_lessons/alishevExtendedJava/SemaphoreTest.java
+++ src/com/company/concurrency_lessons/alishevExtendedJava/SemaphoreTest.java
@@ -1,9 +1,49 @@
 package com.company.concurrency_lessons.alishevExtendedJava;
 
+import com.company.concurrency_lessons.vasko.ColorScheme;
+
 import java.util.concurrent.Semaphore;
 
 public class SemaphoreTest {
     public static void main(String[] args) {
-        Semaphore semaphore = new Semaphore(3);
+
+
+
+       // Semaphore semaphore = new Semaphore(3);
+        /*try {
+            semaphore.acquire();
+            semaphore.acquire();
+            semaphore.acquire();
+            System.out.println("All permits have been acquired");
+            semaphore.release();
+            semaphore.acquire();
+            System.out.println("Can't reach here...");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //semaphore.release();
+        System.out.println(semaphore.availablePermits());*/
+    }
+}
+
+class Connection {
+    private static Connection connection = new Connection();
+    private int connectionsCount = 0;
+
+    private Connection() {
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+    public void doWork() throws InterruptedException {
+        synchronized (this) {
+            connectionsCount++;
+            System.out.println(ColorScheme.GREEN+"ConnectionsCount is " + connectionsCount);
+        }
+        Thread.sleep(5000);
+        synchronized (this) {
+            connectionsCount--;
+        }
     }
 }
