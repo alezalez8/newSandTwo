7b63fc0f72cdd49d6d1024f9ce4f69e87ecc3c6d
alezalez8 <5446943@ukr.net>
2021-10-31 20:14:37 +0200

    alishev CountDownLatch full

---
+++ src/com/company/concurrency_lessons/alishevExtendedJava/CountDownLatchTest.java
@@ -0,0 +1,49 @@
+package com.company.concurrency_lessons.alishevExtendedJava;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchTest {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 3; i++) {
+            executorService.submit(new Processor(i, countDownLatch));
+        }
+        executorService.shutdown();
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(1000);
+            countDownLatch.countDown();
+        }
+        /*
+        countDownLatch.await();
+        System.out.println("Latch has been opened, main thread is proceccing");
+*/    }
+}
+
+class Processor implements Runnable {
+    private int id;
+    private CountDownLatch countDownLatch;
+
+    public Processor(int id, CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread with id " + id + " proceeded.");
+
+    }
+}
