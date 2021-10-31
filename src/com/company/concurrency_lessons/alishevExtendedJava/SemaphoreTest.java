13bcd0fbf532df3ed6398b89f256ce775274a71c
alezalez8 <5446943@ukr.net>
2021-10-31 22:08:28 +0200

    alishev Semahpore begin

---
+++ src/com/company/concurrency_lessons/alishevExtendedJava/SemaphoreTest.java
@@ -0,0 +1,9 @@
+package com.company.concurrency_lessons.alishevExtendedJava;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(3);
+    }
+}
