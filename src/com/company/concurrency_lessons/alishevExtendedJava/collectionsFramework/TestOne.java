24777914962bb660db5e9d1425c70019523fd031
alezalez8 <5446943@ukr.net>
2021-11-02 19:06:45 +0200

    end of multhithreading

--- src/com/company/concurrency_lessons/alishevExtendedJava/collectionsFramework/TestOne.java
+++ src/com/company/concurrency_lessons/alishevExtendedJava/collectionsFramework/TestOne.java
@@ -1,4 +1,47 @@
 package com.company.concurrency_lessons.alishevExtendedJava.collectionsFramework;
 
+import java.util.Random;
+import java.util.concurrent.*;
+
 public class TestOne {
+    //  private static int result;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+
+        Future<Integer> future = executorService.submit(() -> {
+            System.out.println("Starting");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Finished");
+
+            Random random = new Random();
+            int randomValue = random.nextInt(10);
+            if (randomValue < 5) {
+                System.out.println("------" + randomValue);
+                throw new Exception("Something bad happened");
+            }
+            return randomValue;
+        });
+
+        executorService.shutdown();
+
+        try {
+            int result = future.get();
+            System.out.println("Result is " + result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (
+                ExecutionException e) {
+            Throwable ex = e.getCause();
+            System.out.println(ex.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
 }
