6f2256d59ca6a4941f39d5aa54045d21a10d5875
alezalez8 <5446943@ukr.net>
2021-11-02 20:15:20 +0200

    end lesson2

--- src/com/company/concurrency_lessons/alishevExtendedJava/collectionsFramework/Lesson2.java
+++ src/com/company/concurrency_lessons/alishevExtendedJava/collectionsFramework/Lesson2.java
@@ -1,7 +1,22 @@
 package com.company.concurrency_lessons.alishevExtendedJava.collectionsFramework;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Lesson2 {
     public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            arrayList.add(i);
+        }
+        arrayList.remove(10);
+        System.out.println(arrayList);
+        System.out.println(arrayList.get(10));
+
+        arrayList = new LinkedList<>();
+        System.out.println(arrayList);
+
 
     }
 }
