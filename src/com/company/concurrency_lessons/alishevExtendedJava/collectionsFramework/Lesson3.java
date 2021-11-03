ce62ef23633af9864029b6050648288a7fa8160e
alezalez8 <5446943@ukr.net>
2021-11-03 06:41:17 +0200

    lesson 5 end

--- src/com/company/concurrency_lessons/alishevExtendedJava/collectionsFramework/Lesson3.java
+++ src/com/company/concurrency_lessons/alishevExtendedJava/collectionsFramework/Lesson3.java
@@ -1,4 +1,11 @@
 package com.company.concurrency_lessons.alishevExtendedJava.collectionsFramework;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Lesson3 {
+
+    public static void main(String[] args) {
+        List<Integer> linkedList = new LinkedList<>();
+    }
 }
