952b00f2843dadad44df760b2e8f95fea26a8d56
alezalez8 <5446943@ukr.net>
2022-01-19 22:13:31 +0200

    testMethod

--- src/com/company/blackjava/stream/Test3.java
+++ src/com/company/blackjava/stream/Test3.java
@@ -19,8 +19,23 @@ public class Test3 {
         list.add(12);
         list.add(18);
 
-        System.out.println(list);
-        System.out.println(list.stream().reduce((k, v) -> k * v).get());
+        //System.out.println(list);
+       // System.out.println(list.stream().reduce((k, v) -> k * v).get());
+
+        int result = list.stream().reduce(1, (accum, element) ->
+                accum * element);
+        System.out.println("Result is " + result);
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("priver");
+        list2.add("kak dela");
+        list2.add("normalno");
+        list2.add("poka");
+
+
+        String result3 = list2.stream().reduce((a,e) ->
+                a + " " +e ).get();
+        System.out.println(result3);
 
     }
 }
