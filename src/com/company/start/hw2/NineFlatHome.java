33f7a2952998a2bd9b7d66c1d5c5d871f1717e74
alezalez8 <5446943@ukr.net>
2022-01-21 15:20:54 +0200

    hw2_2 end

--- src/com/company/start/hw2/NineFlatHome.java
+++ src/com/company/start/hw2/NineFlatHome.java
@@ -1,7 +1,29 @@
 package com.company.start.hw2;
 
+import java.util.Scanner;
+
 public class NineFlatHome {
     public static void main(String[] args) {
+        int entrance = 4;
+        int flat = 9;
+        int roomOnFlat = 4;
+        int accountApartInEntrance;
+        int inputApartment;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input number of apartment");
+        inputApartment = scanner.nextInt();
+        if (0 < inputApartment && inputApartment <= 144) {
+
+            accountApartInEntrance = roomOnFlat * flat;
+
+            System.out.printf("Apartment is in %d entrance and on the %d floor",
+                    (inputApartment - 1) / (accountApartInEntrance) + 1,
+                    (inputApartment - 1) % (accountApartInEntrance) / roomOnFlat + 1);
+
+        } else {
+            System.out.println("You input wrong number of apartment");
+        }
 
     }
 }
