db71cd5fc3acc92b39eff9ec50d3dd804e387812
alezalez8 <5446943@ukr.net>
2022-01-21 15:46:25 +0200

    hw2_3 end

--- src/com/company/start/hw2/DetermineYear.java
+++ src/com/company/start/hw2/DetermineYear.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class DetermineYear {
 
     public static void main(String[] args) {
+        int inputYear;
 
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input year");
+        inputYear = scanner.nextInt();
+        if (inputYear % 4 == 0 &&
+                ((inputYear % 100 != 0) || (inputYear % 400 == 0))
+        ) {
+            System.out.println("This is the leap year!");
+        } else {
+            System.out.println("This is not a leap year ");
+        }
     }
 }
