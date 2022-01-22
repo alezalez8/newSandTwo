f60ee29f113c042801b114380c059a4c59ec8dea
alezalez8 <5446943@ukr.net>
2022-01-22 13:58:02 +0200

    hw2 lev_2 polindrom end

--- src/com/company/start/hw2/Pallindrom.java
+++ src/com/company/start/hw2/Pallindrom.java
@@ -1,4 +1,30 @@
 package com.company.start.hw2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Pallindrom {
+
+
+    public static void main(String[] args) {
+        int inputNumber;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input six-digits number");
+        try {
+            inputNumber = scanner.nextInt();
+
+            if (inputNumber / 100_000 == inputNumber % 10
+                    && inputNumber % 100_000 / 10_000 == inputNumber % 100 / 10
+                    && inputNumber % 10_000 / 1000 == inputNumber % 1000 / 100) {
+                System.out.println("This number is polindrom");
+            } else {
+                System.out.println("This number is not polindrom");
+            }
+
+        } catch (InputMismatchException exception) {
+            System.out.println("You input no six-digits number");
+        }
+
+    }
 }
