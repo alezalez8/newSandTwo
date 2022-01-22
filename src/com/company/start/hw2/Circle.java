91c15187fa5cf6ea6add6ab96f33e17be52c965b
alezalez8 <5446943@ukr.net>
2022-01-22 13:36:25 +0200

    hw2 lev_2 Circle end

--- src/com/company/start/hw2/Circle.java
+++ src/com/company/start/hw2/Circle.java
@@ -1,5 +1,29 @@
 package com.company.start.hw2;
 
+import java.util.Scanner;
+
 public class Circle {
+    public static void main(String[] args) {
+        double radius = 4d;
+        int x;
+        int y;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input x");
+        x = scanner.nextInt();
+        System.out.println("Input y");
+        y = scanner.nextInt();
+
+        if (Math.sqrt(x * x + y * y) <= radius) {
+            System.out.println("Point with coord x = " + x + ", y = " + y
+                    + " is insdide circle");
+        } else {
+            System.out.println("Point with coord x = " + x + ", y = " + y
+                    + " is outsdide circle");
+        }
+
+
+    }
+
 
 }
