585e28c927bf9a67e30713b61444a8e00047a175
alezalez8 <5446943@ukr.net>
2022-01-25 21:43:26 +0200

    hw3 level 1 task 3 is done

---
+++ src/com/company/start/hw3/EmptyRectangle.java
@@ -0,0 +1,24 @@
+package com.company.start.hw3;
+
+import java.util.Scanner;
+
+public class EmptyRectangle {
+    public static void main(String[] args) {
+
+
+        int high;
+        int length;
+        int lowBound = 4;
+        int highBond = 16;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input high of rectangle");
+        high = scanner.nextInt();
+        System.out.println("Please input length of rectangle");
+        length = scanner.nextInt();
+
+        for (int i = 0; i < high; i++) {
+
+        }
+    }
+}
---
+++ src/com/company/start/hw3/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package com.company.start.hw3;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 5;
+
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i + " x " + number + " = " + i * number);
+        }
+    }
+}
