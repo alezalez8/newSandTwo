a1fb0dec30481405f0e398f9d96f0f08e34f08e3
alezalez8 <5446943@ukr.net>
2022-01-26 10:36:22 +0200

    hw3 level 2 task 2 end

--- src/com/company/start/hw3/NaturalNumber.java
+++ src/com/company/start/hw3/NaturalNumber.java
@@ -4,22 +4,17 @@ public class NaturalNumber {
     public static void main(String[] args) {
         int number = 100;
 
-
-      /*  for (int i = 1; i <= 37; i++) {
-            System.out.println(37 % i);
-        }*/
-
+        System.out.println("List of simple numbers:");
         for (int i = 2; i <= number; i++) {
             for (int j = 2; j <= i; j++) {
-                if (i % j == 0 && i == j) {
-                    System.out.print(i + "  ");
-
+                if (i % j == 0) {
+                    if (i == j) {
+                        System.out.print(i + "  ");
+                    } else {
+                        break;
+                    }
                 }
-
-
             }
         }
-
     }
 }
-// 37
