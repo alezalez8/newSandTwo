eeca0bb55a2efa2418fcb2281a32156a81f45bd1
alezalez8 <5446943@ukr.net>
2022-01-31 23:28:08 +0200

    number to string half end

--- src/com/company/start/hw4/lesson/Sand2.java
+++ src/com/company/start/hw4/lesson/Sand2.java
@@ -1,10 +1,16 @@
 package com.company.start.hw4.lesson;
 
+import java.util.Scanner;
+
 public class Sand2 {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
             System.out.print("ostatok ot " + i + " = " + i % 3);
             System.out.println(";  " + i / 3 + " segmeny");
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Helloy");
         }
     }
 }
