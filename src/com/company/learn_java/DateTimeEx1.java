6be1ed11c7c12c3921882017e8914dfc35f5821b
alezalez8 <5446943@ukr.net>
2022-02-03 15:16:58 +0200

    number to string test two

---
+++ src/com/company/learn_java/DateTimeEx1.java
@@ -0,0 +1,41 @@
+package com.company.learn_java;
+
+import java.time.*;
+
+public class DateTimeEx1 {
+    public static void main(String[] args) {
+
+        LocalDate ld = LocalDate.of(2022, 1, 22);
+        LocalDate ld2 = LocalDate.of(2022, 2, 25);
+        System.out.println(ld);
+        Duration dur = Duration.ofHours(5);
+        LocalTime localTime = LocalTime.of(15, 20);
+        System.out.println(localTime);
+        localTime = localTime.plus(dur);
+        System.out.println(localTime);
+
+       // ld = ld.plusDays(1600).minusYears(3);
+        // System.out.println(ld.isBefore(ld2));
+
+        System.out.println();
+
+
+
+
+
+       /* LocalDate ld2 = LocalDate.of(2022, Month.JANUARY, 12);
+        System.out.println(ld2);
+        System.out.println(Month.MARCH.getValue());
+        LocalTime localTime = LocalTime.of(12, 45, 00);
+        System.out.println(localTime);
+        System.out.println("---------------------------");
+
+        LocalDateTime time = LocalDateTime.of(ld, localTime);
+        System.out.println(time);*/
+
+
+
+    }
+
+}
+
