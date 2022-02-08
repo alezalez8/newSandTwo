3444a17f46114f011101508157fa2c33e4599a16
alezalez8 <5446943@ukr.net>
2022-02-08 15:32:04 +0200

    blackjava lesson 103 2:48

---
+++ src/com/company/blackjava/nio/PathAndFilesEx1.java
@@ -0,0 +1,11 @@
+package com.company.blackjava.nio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathAndFilesEx1 {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("test5.txt");
+        Path folderPath = Paths.get();
+    }
+}
