66788c150b9b3f723524618151e7ac5ced98cf2a
alezalez8 <5446943@ukr.net>
2022-03-10 18:49:40 +0200

    java_start fix bug in hw5 NumberCombin

--- src/com/company/start/hw5/NumberCombin.java
+++ src/com/company/start/hw5/NumberCombin.java
@@ -6,13 +6,11 @@ import java.util.Scanner;
 public class NumberCombin {
     public static void main(String[] args) {
         int size;
-        // int[] testArray = {1, 2, 3, 4};
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter size of array");
         size = scanner.nextInt();
         scanner.close();
         availableAmountOfCombination(createArray(size));
-
     }
 
     public static void availableAmountOfCombination(int[] array) {
@@ -20,10 +18,10 @@ public class NumberCombin {
         int innerSize = size;
 
         System.out.println(Arrays.toString(array));
-        // for(int i = size; i > 0; )
+        // for(int i = size; i > 0; ) {
         for (; size > 0; ) {
             innerLoop(array, size);
-            if (array[size] != size) {
+            if (array[size] <= size) {
                 System.out.println(Arrays.toString(array));
                 size = innerSize;
             } else {
@@ -37,10 +35,10 @@ public class NumberCombin {
     public static void innerLoop(int[] array, int size) {
         int temp = array[0];
         for (int i = 0; i < size; i++) {
-            //   array[i - 1] = array[i];
+             //  array[i - 1] = array[i];
             array[i] = array[i + 1];
         }
-        //array[size - 1] = temp;
+       // array[size - 1] = temp;
         array[size] = temp;
     }
 
